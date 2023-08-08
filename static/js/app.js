ef1897c71977d3943dc3262fe8ea05e46af61667
hhodek <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>
2023-08-08 14:24:00 -0500

    interactivity fix

--- static/js/app.js
+++ static/js/app.js
@@ -3,142 +3,87 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
   .then(function(data) {
     const names = data.names;
     const samples = data.samples;
+    const metadata = data.metadata;
     const dropdown = d3.select("#selDataset");
 
     // Populate the dropdown with sample names
     names.forEach(function(name) {
-      dropdown.append("option").text(name).property("value", name);
-    });
-
-    // Function to update the chart based on selected sample
-    function updateChart(selectedSample) {
-      const selectedData = samples.find(sample => sample.id === selectedSample);
-      const sampleValues = selectedData.sample_values.slice(0, 10).reverse();
-      const otuIds = selectedData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
-      const otuLabels = selectedData.otu_labels.slice(0, 10).reverse();
-
-      const trace = {
-        type: "bar",
-        orientation: "h",
-        x: sampleValues,
-        y: otuIds,
-        text: otuLabels,
-        hoverinfo: "text",
-      };
-
-      const layout = {
-        title: "Top 10 OTUs",
-        xaxis: { title: "Sample Values" },
-      };
-
-      const data = [trace];
-
-      // Corrected target ID: "bar" instead of "barChart"
-      Plotly.newPlot("bar", data, layout);
-    }
-
-    // Initial chart update
-    updateChart(names[0]);
-
-    // Event listener for dropdown change
-    dropdown.on("change", function() {
-      const selectedSample = dropdown.property("value");
-      updateChart(selectedSample);
-    });
-  })
-  .catch(function(error) {
-    console.error("Error loading the JSON file:", error);
-  });
-
- // Fetch JSON data
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-.then(function(data) {
-  const names = data.names;
-  const samples = data.samples;
-  const metadata = data.metadata;
-  const dropdown = d3.select("#selDataset");
-
-  // Populate the dropdown with sample names
-  names.forEach(function(name) {
-    dropdown.append("option").text(name).property("value", name);
-  });
-
-  // Function to update the charts based on selected sample
-  function updateCharts(selectedSample) {
-    const selectedData = samples.find(sample => sample.id === selectedSample);
+        dropdown.append("option").text(name).property("value", name);
+      });
+  
+      // Function to update the chart based on selected sample
+      function updateChart(selectedSample) {
+        const selectedData = samples.find(sample => sample.id === selectedSample);
+        const sampleValues = selectedData.sample_values.slice(0, 10).reverse();
+        const otuIds = selectedData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
+        const otuLabels = selectedData.otu_labels.slice(0, 10).reverse();
+  
+        const trace = {
+          type: "bar",
+          orientation: "h",
+          x: sampleValues,
+          y: otuIds,
+          text: otuLabels,
+          hoverinfo: "text",
+        };
+  
+        const layout = {
+          title: "Top 10 OTUs",
+          xaxis: { title: "Sample Values" },
+        };
+  
+        const data = [trace];
+  
+        // Corrected target ID: "bar" instead of "barChart"
+        Plotly.newPlot("bar", data, layout);
+      }
+  
+      // Function to update the charts based on selected sample
+      function updateCharts(selectedSample) {
+        const selectedData = samples.find(sample => sample.id === selectedSample);
+        const selectedMetadata = metadata.find(sample => sample.id === parseInt(selectedSample));
+  
+        // Bubble Chart
+        const traceBubble = {
+          x: selectedData.otu_ids,
+          y: selectedData.sample_values,
+          text: selectedData.otu_labels,
+          mode: "markers",
+          marker: {
+            size: selectedData.sample_values,
+            color: selectedData.otu_ids,
+            colorscale: "Earth",
+          },
+        };
+  
+        const layoutBubble = {
+          title: "OTU Bubble Chart",
+          xaxis: {
+            title: "OTU ID",
+            dtick: 500, // Set the interval between x-axis tick values to 500
+          },
+          yaxis: { title: "Sample Values" },
+        };
+  
+        const dataBubble = [traceBubble];
+  
+        Plotly.newPlot("bubble", dataBubble, layoutBubble);
+  
+        // Demographic Info
+        const demographicInfo = selectedMetadata ? Object.entries(selectedMetadata) : [];
+        const demographicPanel = d3.select("#sample-metadata");
+        demographicPanel.html(""); // Clear previous content
+  
+        demographicInfo.forEach(([key, value]) => {
+          demographicPanel.append("p").text(`${key}: ${value}`);
+        });
+      }
+  
+      // Function to update the gauge chart
+function updateGaugeChart(selectedSample) {
     const selectedMetadata = metadata.find(sample => sample.id === parseInt(selectedSample));
-
-    // Bubble Chart
-    const traceBubble = {
-      x: selectedData.otu_ids,
-      y: selectedData.sample_values,
-      text: selectedData.otu_labels,
-      mode: "markers",
-      marker: {
-        size: selectedData.sample_values,
-        color: selectedData.otu_ids,
-        colorscale: "Earth",
-      },
-    };
-
-    const layoutBubble = {
-      title: "OTU Bubble Chart",
-      xaxis: {
-        title: "OTU ID",
-        dtick: 500, // Set the interval between x-axis tick values to 500
-      },
-      yaxis: { title: "Sample Values" },
-    };
-
-    const dataBubble = [traceBubble];
-
-    Plotly.newPlot("bubble", dataBubble, layoutBubble);
-
-    // Demographic Info
-    const demographicInfo = selectedMetadata ? Object.entries(selectedMetadata) : [];
-    const demographicPanel = d3.select("#sample-metadata");
-    demographicPanel.html(""); // Clear previous content
-
-    demographicInfo.forEach(([key, value]) => {
-      demographicPanel.append("p").text(`${key}: ${value}`);
-    });
-  }
-
-  // Initial chart update
-  updateCharts(names[0]);
-
-  // Event listener for dropdown change
-  dropdown.on("change", function() {
-    const selectedSample = dropdown.property("value");
-    updateCharts(selectedSample);
-  });
-})
-.catch(function(error) {
-  console.error("Error loading the JSON file:", error);
-});
-
-
-   // Gauge Chart
-
-  // Fetch JSON data
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-.then(function(data) {
-  const names = data.names;
-  const samples = data.samples;
-  const metadata = data.metadata;
-  const dropdown = d3.select("#selDataset");
-
-  // Populate the dropdown with sample names
-  names.forEach(function(name) {
-    dropdown.append("option").text(name).property("value", name);
-  });
-
-  // Function to update the charts based on selected sample
-  function updateCharts(selectedSample) {
-    const selectedData = samples.find(sample => sample.id === selectedSample);
-    const selectedMetadata = metadata.find(sample => sample.id === parseInt(selectedSample));
-
-    // Bar Chart (similar to your existing code)
+  
+    // Gauge Chart
     const washingFrequency = selectedMetadata ? selectedMetadata.wfreq : 0; // Update this to get washing frequency
     const gaugeData = [
       {
@@ -179,15 +124,43 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
     Plotly.newPlot("gauge", gaugeData, layoutGauge);
   }
 
-  // Initial chart update
-  updateCharts(names[0]);
-
-  // Event listener for dropdown change
-  dropdown.on("change", function() {
-    const selectedSample = dropdown.property("value");
-    updateCharts(selectedSample);
-  });
-})
-.catch(function(error) {
-  console.error("Error loading the JSON file:", error);
-});
+  
+      // Function to handle dropdown change event
+      function optionChanged(selectedSample) {
+        // Call the necessary update functions here based on the selected sample
+        updateChart(selectedSample);
+        updateCharts(selectedSample);
+        updateGaugeChart(selectedSample);
+      }
+  
+      // Initial chart update
+      updateCharts(names[0]);
+  
+      // Event listener for dropdown change
+      dropdown.on("change", function() {
+        const selectedSample = dropdown.property("value");
+        optionChanged(selectedSample); // Call optionChanged function
+      });
+    })
+    .catch(function(error) {
+      console.error("Error loading the JSON file:", error);
+    });
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    
